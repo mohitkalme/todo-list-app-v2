ef694b81bcddb27d788ff640732b00089b31ef71
Mohit kalme <68311332+mohitkalme@users.noreply.github.com>
2023-05-06 18:04:00 +0530

    adding vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,30 @@
-import '@/styles/globals.css'
-import 'intro.js/introjs.css'
-import 'react-toastify/dist/ReactToastify.css';
+import "@/styles/globals.css";
+import "intro.js/introjs.css";
+import "react-toastify/dist/ReactToastify.css";
 
-import type { AppProps } from 'next/app'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 //react-toastify
 import { ToastContainer } from "react-toastify";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+//Analytics
+import { Analytics } from "@vercel/analytics/react";
 
-
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
         <>
           <Component {...pageProps} />
+          <Analytics />
 
           <ToastContainer
             position="bottom-left"
@@ -37,8 +39,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
             theme="light"
           />
         </>
-
       </SessionProvider>
     </QueryClientProvider>
-  )
+  );
 }
